dd7d9248d52a25a4d2eff81e9f3d2fc2237f0f1f
Juliana Correa <jucorrea.mail@gmail.com>
2013-01-20 10:05:27 -0200

    Learning activity lifecycle, saving context before destroying activity and recovering it later  Learning to nest layouts

--- src/br/com/teoriadetudo/android/helloWorld/DisplayMessageActivity.java
+++ src/br/com/teoriadetudo/android/helloWorld/DisplayMessageActivity.java
@@ -14,25 +14,31 @@ public class DisplayMessageActivity extends Activity {
     /** Called when the activity is first created. */
     @SuppressLint("NewApi")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
         // ActionBar API needs honeycomb or greater
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
             getActionBar().setDisplayHomeAsUpEnabled(true);
+            getActionBar().setDisplayShowTitleEnabled(true);
+        }
 
+        // Receive information from the intent creator
         Intent intent = getIntent();
         String message = intent.getStringExtra(HelloWorldActivity.EXTRA_MESSAGE);
 
+        // Layout for this Activity consists only of a textview
         TextView textView = new TextView(this);
         textView.setTextSize(40);
         textView.setText(message);
-
         setContentView(textView);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
+        // Return to app home
         switch (item.getItemId()) {
             case android.R.id.home:
                 NavUtils.navigateUpFromSameTask(this);
--- src/br/com/teoriadetudo/android/helloWorld/HelloWorldActivity.java
+++ src/br/com/teoriadetudo/android/helloWorld/HelloWorldActivity.java
@@ -1,24 +1,52 @@
 package br.com.teoriadetudo.android.helloWorld;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class HelloWorldActivity extends Activity
 {
     public final static String EXTRA_MESSAGE = "br.com.teoriadetudo.android.helloWorld.MESSAGE";
-
+    static final String ROTATIONS = "number";
+    private int curNumber;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        // Make sure we're running on Honeycomb or higher to use ActionBar APIs
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // In main activity, the app icon in the action bar is not a button
+            ActionBar actionBar = getActionBar();
+            actionBar.setHomeButtonEnabled(false);
+        }
+
+        if (savedInstanceState != null) {
+            curNumber = savedInstanceState.getInt(ROTATIONS, curNumber);
+        } else {
+            curNumber = 0;
+        }
+
+        // Set the user interface layout for this Activity
+        // The layout file is defined in the project res/layout/main_activity.xml file
         setContentView(R.layout.main);
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState)
+    {
+        savedInstanceState.putInt(ROTATIONS, curNumber + 1);
+        TextView textView = (TextView) findViewById(R.id.count_text);
+        if (textView != null) textView.setText("");
+        super.onSaveInstanceState(savedInstanceState);
+    }
 
     /** Called when the user clicks the Send button */
     public void sendMessage(View view)
@@ -27,5 +55,11 @@ public class HelloWorldActivity extends Activity
         EditText editText = (EditText) findViewById(R.id.edit_message);
         intent.putExtra(EXTRA_MESSAGE, editText.getText().toString());
         startActivity(intent);
+    }
+
+    public void countNumber(View view)
+    {
+        TextView textView = (TextView) findViewById(R.id.count_text);
+        if (textView != null) textView.setText(String.valueOf(curNumber));
     }
 }
